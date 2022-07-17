fa4abe909b8847c2d687a89d41a8f3c3dba94448
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-17 17:22:46 -0400

    questions populating when console logged

---
+++ Develop/index.js
@@ -0,0 +1,71 @@
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+const fs = require('fs');
+const imports = require('./utils.generateMarkdown')
+// const Choices = require('inquirer/lib/objects/choices');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        question: 'What would you like the title to be?',
+    },
+    {
+        type: 'input',
+        question: 'Add a description.',
+    },
+    {
+        type: 'input',
+        question: 'Add installtion instructions.',
+    },
+    {
+        type: 'input',
+        question: 'Add usage information.',
+    },
+    {
+        type: 'input',
+        question: 'Add contribution guidelines.',
+    },
+    {
+        type: 'input',
+        question: 'Add test instructions.',
+    },
+    {
+        // switch statement
+        type: 'checkbox',
+        question: 'Pick a license.',
+        choices: ['Public domain', 'Permissive', 'LGPL', 'Copyleft', 'Proprietary'],
+    },
+    {
+        type: 'input',
+        question: 'What is your GitHub Username?',
+    },
+    {
+        type: 'input',
+        question: 'Add the link to your GitHub profile.',
+    },
+    {
+        type: 'input',
+        question: 'Add your email.',
+    },
+    {
+        type: 'input',
+        question: 'Describe the best way to get in contact with you.',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(generateMarkdown, )
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    console.log(questions)
+}
+//     .then((answers) => fs.writeToFile)
+// }
+
+// Function call to initialize app
+init();
